1e64e083117445a081c18baa9b05f0a5abcf86a5
raphael meyer <raphael.akira.meyer@gmail.com>
2021-05-21 22:52:45 +0200

    add command line parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import Options.Applicative
+
+data Actions = Play | Pause | PlayPause | CurrentSong deriving (Eq, Show)
+
+actions :: Parser Actions
+actions =
+  hsubparser
+    ( command "play" (info (pure Play) (progDesc "Start playback"))
+        <> command "pause" (info (pure Pause) (progDesc "Pause playback"))
+        <> command "toggle" (info (pure PlayPause) (progDesc "Toggle play and pause"))
+        <> commandGroup "Playback commands:"
+        <> metavar "Playback COMMAND"
+    )
+    <|> hsubparser
+      ( command "daemon" (info (pure CurrentSong) (progDesc "Start daemon"))
+          <> commandGroup "Daemon commands:"
+          <> metavar "Daemon COMMAND"
+      )
+
+options :: ParserInfo Actions
+options =
+  info
+    (actions <**> helper)
+    ( fullDesc
+        <> progDesc "Spotify CLI Tool"
+        <> header "Control spotify client from the command line."
+    )
+
 main :: IO ()
-main = do
-  return ()
+main = execParser options >>= print
